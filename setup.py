1fa1498e83e1b7f1eb07d34e9c27540e5c98cb9e
Hocine <hocine@sanbi.ac.za>
2016-02-26 17:39:01 +0200

    bika.lims without version

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(name='bika.health',
       zip_safe=False,
       install_requires=[
           'setuptools',
-          'bika.lims<3.2',
+          'bika.lims',
           'archetypes.schemaextender',
           'collective.wtf',
       ],
